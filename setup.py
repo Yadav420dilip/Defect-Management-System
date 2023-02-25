035342bd88b123bf3fe4acdf99b925f207345769
Dilip Yadav <dy7208451003@gmail.com>
2023-02-25 22:47:35 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Defect Management System',
+    version='',
+    packages=['Defect_Management'],
+    url='',
+    license='',
+    author='Dilip',
+    author_email='',
+    description=''
+)
